1290159a8d9fe3db5b97cc9e8a56e3dff0674447
Andrea Isla Collins-Hed <33668042+acollins-hed@users.noreply.github.com>
2025-03-12 19:54:57 -0700

    Revised screen output from initialize_variables.cpp on running code to be more clear and accurate.

--- src/initialize_variables.cpp
+++ src/initialize_variables.cpp
@@ -518,7 +518,7 @@ int initialize_variables(int argc, char* argv[], struct Common_Variables * commo
   tm *ltm = localtime(&begin);
   log_file<<"Started at "<<1900+ltm->tm_year<<"-"<<1+ltm->tm_mon<<"-"<<ltm->tm_mday<<" "<<ltm->tm_hour<<":"<<ltm->tm_min<<":"<<ltm->tm_sec<<std::endl;
   log_file<<"Saved in files: "<<common_variables->output_filename<<std::endl;
-  std::cout<<"Copyleft (2019) A.I. Collins-Hed\nAll Wrongs Reversed.\nPlease cite Collins-Hed et al. 2019 in published works using this software.\n";
+  std::cout<<"Copyleft (2025) A.I. Collins-Hed\nAll Wrongs Reversed.\nPlease cite Collins-Hed et al. 2019 in published works using this software.\n";
   std::cout<<"\n--------------------------------------\nn (number of sites per interface) is "<<common_variables->N_int_interface<<std::endl;
   log_file<<"Interaction interface width (bits): n = "<<common_variables->N_int_interface<<std::endl;
   //std::cout<<"k (number of sites for full energy) is "<<k<<std::endl;
@@ -531,7 +531,7 @@ int initialize_variables(int argc, char* argv[], struct Common_Variables * commo
   log_file<<"Number of distinct aaRSs: A = "<<common_variables->N_aaRS<<std::endl;
   //std::cout<<"A starting (beginning number of aaRSs) is "<<Acap<<std::endl;
   //log_file<<"Number of beginning aaRSs: Acap = "<<Acap<<std::endl;
-  std::cout<<"S (number of site-type) is "<<common_variables->N_site_type<<std::endl;
+  std::cout<<"S (number of site-types) is "<<common_variables->N_site_type<<std::endl;
   log_file<<"Number of distinct site-types: S = "<<common_variables->N_site_type<<std::endl;
   std::cout<<"Site-Type frequencies are ";
   log_file<<"Site-Type frequencies: ";
@@ -558,23 +558,33 @@ int initialize_variables(int argc, char* argv[], struct Common_Variables * commo
   log_file<<"Population size: N = "<<common_variables->N_population<<std::endl;
   if(common_variables->rate)
     {
-      log_file<<"rate dependent\n";
-      std::cout<<"rate dependent\n";
+      log_file<<"Rate dependent\n";
+      std::cout<<"Rate dependent\n";
     }
   else
     {
-      log_file<<"rate independent\n";
-      std::cout<<"rate indepdent\n";
+      log_file<<"Rate independent\n";
+      std::cout<<"Rate indepdent\n";
+    }
+  if(common_variables->mask)
+    {
+      log_file<<"Masking applied\n";
+      std::cout<<"Masking applied\n";
+    }
+  else
+    {
+      log_file<<"No masking applied\n";
+      std::cout<<"No masking applied\n";
     }
   if(common_variables->proofreading)
     {
-      log_file<<"proofreading applied\n";
-      std::cout<<"proofreading applied\n";
+      log_file<<"Proofreading applied\n";
+      std::cout<<"Proofreading applied\n";
     }
   else
     {
-      log_file<<"no proofreading\n";
-      std::cout<<"no proofreading\n";
+      log_file<<"No proofreading\n";
+      std::cout<<"No proofreading\n";
     }
   
   std::cout<<"The rate selection is "<<common_variables->rate_constant<<std::endl;
@@ -595,12 +605,12 @@ int initialize_variables(int argc, char* argv[], struct Common_Variables * commo
       }
   }
 
-  log_file<<"Translation machinery substitution rate: mu = "<<common_variables->mu_id_feat<<std::endl;
-  std::cout<<"mu (translation machinery substitution rate) is "<<common_variables->mu_id_feat<<std::endl;
+  log_file<<"Mutation rate of identifying features and epsitatic modifiers: mu = "<<common_variables->mu_id_feat<<std::endl;
+  std::cout<<"mu (mutation rate of identifying features and epistatic modifiers)is "<<common_variables->mu_id_feat<<std::endl;
   log_file<<"Codon substitution rate: Mu = "<<common_variables->mu_per_codon<<std::endl;
   std::cout<<"Mu (codon substitution rate) is "<<common_variables->mu_per_codon<<std::endl;
-  log_file<<"Transition bias: kappa = "<<common_variables->transition_bias<<std::endl;
-  std::cout<<"kappa (transition bias) "<<common_variables->transition_bias<<std::endl;
+  log_file<<"Transition bias: "<<common_variables->transition_bias<<std::endl;
+  std::cout<<"Transition bias: "<<common_variables->transition_bias<<std::endl;
   if(common_variables->codon_ring_space){
     log_file<<"Codon ring space.\n";
     std::cout<<"Codon ring space.\n";
@@ -617,9 +627,9 @@ int initialize_variables(int argc, char* argv[], struct Common_Variables * commo
   log_file<<"Baseline missense fitness: phi = "<<common_variables->phi<<std::endl;
   std::cout<<"phi (the baseline fitness) is "<<common_variables->phi<<std::endl;
   log_file<<"The halting fitness: "<<common_variables->halting_fitness<<std::endl;
-  std::cout<<"fthalt (the halting fitness) is "<<common_variables->halting_fitness<<std::endl;
+  std::cout<<"The halting fitness is "<<common_variables->halting_fitness<<std::endl;
   log_file<<"The halting fixation: "<<common_variables->halting_fixation<<std::endl;
-  std::cout<<"fxhalt (the halting fixation) is "<<common_variables->halting_fixation<<std::endl;
+  std::cout<<"The halting fixation is "<<common_variables->halting_fixation<<std::endl;
   log_file<<"The binomial parameter for genotype initialization: p = "<<common_variables->binom_p<<std::endl;
   std::cout<<"p for the binomial is "<<common_variables->binom_p<<std::endl;
   log_file<<"The number of trajectories: "<<common_variables->N_trajectory<<std::endl;
@@ -628,16 +638,18 @@ int initialize_variables(int argc, char* argv[], struct Common_Variables * commo
   std::cout<<"kmax is "<<common_variables->kmax<<std::endl;
   log_file<<"The minimum dissociation rate: kmin = "<<common_variables->kmin<<std::endl;
   std::cout<<"kmin is "<<common_variables->kmin<<std::endl;
-  log_file<<"Fixations from the Moran Process.\n";
-  std::cout<<"Fixations from the Moran Process.\n";
+  //log_file<<"Fixations from the Moran Process.\n";
+  //std::cout<<"Fixations from the Moran Process.\n";
   if(bl_seed){
-    std::cout<<"PRNG seed: "<<common_variables->seed<<std::endl;
+    std::cout<<"PRNG seed is "<<common_variables->seed<<std::endl;
     log_file<<"PRNG seed: "<<common_variables->seed<<std::endl;
   }
   else{
-    std::cout<<"PRNG seed: "<<random_number_from_random_device<<std::endl;
+    std::cout<<"PRNG seed is "<<random_number_from_random_device<<std::endl;
     log_file<<"PRNG seed: "<<random_number_from_random_device<<std::endl;
   }
+  std::cout<<"Number of threads is "<<common_variables->N_threads<<std::endl;
+  log_file<<"Number of threads: Nthread = "<<common_variables->N_threads<<std::endl;
   std::cout<<"Output file name is \""<<common_variables->output_filename<<"\"\n--------------------------------------\n\n";
   
   log_file.close();
