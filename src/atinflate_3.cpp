32fe7ce957e2698532b2f60a546c36f9f2ebb632
Andrea Isla Collins-Hed <33668042+acollins-hed@users.noreply.github.com>
2026-01-01 20:24:49 -0800

    Added license notice to atinflate_3.cpp. Will add license notice to other files later.

--- src/atinflate_3.cpp
+++ src/atinflate_3.cpp
@@ -1,3 +1,6 @@
+//Copyright (C) 2026 Andrea Collins-Hed
+//See end of file for extended copyright information.
+
 #include<iostream>
 #include<fstream>
 #include<string>
@@ -77,3 +80,25 @@ int main(int argc, char* argv[]){
 
   return 0;
 }
+
+//atinflate_3.cpp is part of atinflate_3
+//atinflate_3 simulates the aaRS-tRNA Interaction Fitness LAndscape
+//Topographer Express (atINFLATE) model, a dynamic extension of the
+//atINFLAT model published in Collins-Hed and Ardell 2019
+//https://www.sciencedirect.com/science/article/pii/S0040580918300789
+//Copyright (C) 2026 Andrea Collins-Hed
+//
+//This file is part of atinflate_3
+//
+//atinflate_3 is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//atinflate_3 is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program. If not, see <https://www.gnu.org/licenses/>. 
